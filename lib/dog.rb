da4b401a8bdae0cd9557ce96907f10f2f7bf7769
Faith  Gicheha <fwgicheh@gmail.com>
2020-06-23 02:11:59 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,13 +3,13 @@ class Dog
   def name=(dogs_name)
    @this_dogs_name = dogs_name
   end
-  def name
-   @this_dogs_name
-  end
-  def bark
-    puts "woof!"
-  end
-end
+#   def name
+#    @this_dogs_name
+#   end
+#   def bark
+#     puts "woof!"
+#   end
+# end
 # fido = Dog.new
 # fido.name ="Fido"
 # puts Fido.name
