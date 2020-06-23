bf760a0fdb5adc5fc44459341f88faddd6f0b720
Faith  Gicheha <fwgicheh@gmail.com>
2020-06-23 02:15:15 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,8 @@
 # Your code goes here!
 class Dog
-  # def name=(dogs_name)
-  #  @this_dogs_name = dogs_name
-  # end
+  def name=(dogs_name)
+   @this_dogs_name = dogs_name
+  end
   # def name
   #  @this_dogs_name
   # end
